2ad03e6ff56ed7cbe47a5269d6a552d7345c49e9
sushant9096 <sushantzope9096@gmail.com>
2019-05-07 13:46:12 +0000

    Initial lineage-16.0

---
+++ lineage_ysl.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from ysl device
+$(call inherit-product, device/xiaomi/ysl/device.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ysl
+PRODUCT_NAME := lineage_ysl
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi S2
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="ysl-user 8.1.0 OPM1.171019.019 V10.0.5.0.ODICNFH release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := xiaomi/ysl/ysl:8.1.0/OPM1.171019.019/V10.0.5.0.ODICNFH:user/release-key
+
