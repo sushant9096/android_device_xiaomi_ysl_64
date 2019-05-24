78d1d3d70511b7463b70ca356d2f20b1d242e9d6
sushant9096 <sushantzope9096@gmail.com>
2019-05-24 19:16:56 +0000

    ysl: implicitly built QTI camera shared libraries
    * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
      flag is enabled.

--- device.mk
+++ device.mk
@@ -120,7 +120,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap
+    Snap \
+	vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
 PRODUCT_PACKAGES += \
