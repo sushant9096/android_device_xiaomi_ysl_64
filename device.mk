7dceef8aba6b14fb130ab26dc9318f87dd061510
Sushant Zope <sushantzope9096@gmail.com>
2019-06-01 23:50:24 +0530

    msm8998-common: build 32-bit android.frameworks.displayservice@1.0  * Needed for camera hal  Change-Id: Ifc1234ac16f1791e227b7a1fc5d17fe5df336592

--- device.mk
+++ device.mk
@@ -118,10 +118,11 @@ PRODUCT_PACKAGES += \
 	
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0_32 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     Snap \
-	vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
 # Configstore
